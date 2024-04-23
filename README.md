2ffff415fc61ab1450f4f2dc496888c0fe6322ac
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-04-23 00:43:55 -0400

    reverted commit, fixed problem, uploaded final README

--- README.md
+++ README.md
@@ -1,43 +1,56 @@
-# sfsdfsd
+# READEME Generator
 
 ## Table of Contents
 
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
 - [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
 - [Questions](#questions)
+
+## Description
+
+![JavaScript](https://img.shields.io/badge/JavaScript-blue) ![Node.js](https://img.shields.io/badge/Node.js-blue) ![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+This application is a README.md generator. You can use this app to dynamically generate a professional README file that you can use on your own projects. This app is quick to install and easy to use. The file generated will only have the information you provide. This ensures a clean and easy to read README! Enjoy!
 
 ## Installation
 
-sdfsdfsf
+Download the index.js file and the utility folder with the generateMarkdown.js file in it. Then, in the Integrated Terminal, install Inquirer by typing ‘npm i inquirer@8.2.4’. Once completed, install colors by typing ‘npm i colors’
 
 ## Usage
 
-dfsfsfdsd
+After downloading the application index.js and the utils folder with the generateMarkdown.js file, in the Integrated Terminal
 
-![Screenshot](sdfsdfs)
+![Screenshot](/images/ss1.png)
 
-## Credits
+Create the package.jason by typing ‘npm init -y’
 
-sdfsdfsdf
+![Screenshot](/images/ss2.png)
 
+Install Inquirer and the colors dependency by typing ‘npm i inquirer@8.2.4’ Do the same thing after this complete but this time type ‘npm i colors’
 
+![Screenshot](/images/ss3.png)
 
-## Contributing
+Once complete, type ‘node index.js’ to start the application
 
-dfsfsdf
+![Screenshot](/images/ss4.png)
 
-## Tests
+Answer the questions in the Integrated Terminal as they appear. If you do not enter a response, that section will not be included in the README.md file
 
-sdfsdfsdf
+![Screenshot](/images/ss5.png)
+
+If correctly completed, you will see a green success message and your README.md will be created!
+
+![Screenshot](/images/ss6.png)
+
+## License
+
+Licensed under the [MIT License](https://opensource.org/licenses/MIT).
 
 ## Questions
 
-[My GitHub Repo](dsfsdfsdf)
+[My GitHub Repo](https://github.com/chrislose23)
 
-[My Email Address](sdfsdfsdf)
+[My Email Address](chrislose23@gmail.com)
 
--- index.js
+++ index.js
@@ -7,95 +7,93 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: colors.magenta('What is the project title?')
+        message: 'What is the project title?'
     },
     {
         type: 'checkbox',
         name: 'badges',
-        message: colors.magenta('What code does your project utilize?'),
+        message: 'What code does your project utilize?',
         choices: [
-            colors.yellow("HTML"), 
-            colors.yellow("CSS"), 
-            colors.yellow("JavaScript"), 
-            colors.yellow("Node.js"), 
-            colors.yellow("Express.js"), 
-            colors.yellow("MySQL"), 
-            colors.yellow("NoSQL"), 
-            colors.yellow("React"), 
+            "HTML", 
+            "CSS", 
+            "JavaScript", 
+            "Node.js", 
+            "Express.js", 
+            "MySQL", 
+            "NoSQL", 
+            "React", 
         ],
     },
     {
         type: 'input',
         name: 'description',
-        message: colors.magenta('Write a description of your project. (What, Why, and How)')
+        message: 'Write a description of your project. (What, Why, and How)'
     },
     {
         type: 'input',
         name: 'install',
-        message: colors.magenta('What are the steps to install your project?')
+        message: 'What are the steps to install your project?'
     },
     {
         type: 'input',
         name: 'credits',
-        message: colors.magenta('List your collaborators and their GitHub profiles.')
+        message: 'List your collaborators and their GitHub profiles.'
     },
     {
         type: 'list',
         name: 'license',
-        message: colors.magenta('What license are you using for your project?'),
+        message: 'What license are you using for your project?',
         choices: [
-            colors.yellow("Apache"), 
-            colors.yellow("GNU General Public"), 
-            colors.yellow("MIT"), 
-            colors.yellow("BSD 2-Clause"), 
-            colors.yellow("BSD 3-Clause"), 
-            colors.yellow("Boost Software"), 
-            colors.yellow("Eclipse Public"), 
-            colors.yellow("Mozilla Public"), 
-            colors.yellow("The Unilicense"),
+            "Apache", 
+            "GNU General Public", 
+            "MIT", 
+            "BSD 2-Clause", 
+            "BSD 3-Clause", 
+            "Boost Software", 
+            "Eclipse Public", 
+            "Mozilla Public", 
+            "The Unilicense",
             ""
-        ]
+        ],
     },
     {
         type: 'input',
         name: 'contribute',
-        message: colors.magenta('How can other developers contribute?')
+        message: 'How can other developers contribute?'
     },
     {
         type: 'input',
         name: 'tests',
-        message: colors.magenta('Provide examples of how to run any tests you created.')
+        message: 'Provide examples of how to run any tests you created.'
     },
     {
         type: 'input',
         name: 'github',
-        message: colors.magenta('What is the link to your GitHub Repo?')
+        message: 'What is the link to your GitHub Repo?'
     },
     {
         type: 'input',
         name: 'email',
-        message: colors.magenta('What is your email address?')
+        message: 'What is your email address?'
     },
     {
         type: 'input',
         name: 'usageInputs',
-        message: colors.magenta('Provide instructions for usage:'),
+        message: 'Provide instructions for usage:',
     },
     {
         type: 'input',
         name: 'usageScreenshot',
-        message: colors.magenta('Provide a screenshot link:'),
+        message: 'Provide a screenshot link:',
         when: (answers) => answers.usageInputs !== undefined,
     },
     {
         type: 'confirm',
         name: 'addMoreUsage',
-        message: colors.magenta('Do you want to add another usage input and screenshot?'),
+        message: 'Do you want to add another usage input and screenshot?',
         default: false,
         when: (answers) => answers.usageInputs !== undefined,
     },
-
-
 ];
 
 function writeToFile(fileName, data) {
@@ -113,23 +111,22 @@ function init() {
         if (answers.usageInputs && answers.usageScreenshot) {
             answers.usageInputs = `${answers.usageInputs}\n\n![Screenshot](${answers.usageScreenshot})`;
         }
-
         function askAdditionalUsage() {
             inquirer.prompt([
                 {
                     type: 'input',
                     name: 'usageInputs',
-                    message: colors.magenta('Provide instructions for usage:'),
+                    message: 'Provide instructions for usage:',
                 },
                 {
                     type: 'input',
                     name: 'usageScreenshot',
-                    message: colors.magenta('Provide a screenshot link:'),
+                    message: 'Provide a screenshot link:',
                 },
                 {
                     type: 'confirm',
                     name: 'addMoreUsage',
-                    message: colors.magenta('Do you want to add another usage input and screenshot?'),
+                    message: 'Do you want to add another usage input and screenshot?',
                     default: false,
                 },
             ]).then((additionalAnswers) => {
