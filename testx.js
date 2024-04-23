6d9965781cb29a6f68437b6a2b7c433bd22460c0
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-04-23 00:06:29 -0400

    Testing

--- testx.js
+++ testx.js
@@ -1,30 +1,51 @@
 function getBadgeURL(license) {
     const licenseBadges = {
-        'Apache': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
-        'GNU General Public': 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
-        'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
-        // Add URLs for other licenses
+      'Apache': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+      'GNU General Public': 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
+      'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+      'BSD 2-Clause': 'https://img.shields.io/badge/License-BSD%202--Clause-orange.svg',
+      'BSD 3-Clause': 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg',
+      'Boost Software': 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg',
+      'Eclipse Public': 'https://img.shields.io/badge/License-EPL%201.0-red.svg',
+      'Mozilla Public': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
+      'The Unilicense': 'https://img.shields.io/badge/license-Unlicense-blue.svg',
     };
+  
     return licenseBadges[license] || '';
   }
+  
   function getLicenseLinkURL(license) {
     const licenseLinks = {
-        'Apache': 'https://opensource.org/licenses/Apache-2.0',
-        'GNU General Public': 'https://www.gnu.org/licenses/gpl-3.0',
-        'MIT': 'https://opensource.org/licenses/MIT',
-        // Add URLs for other licenses
+      'Apache': 'https://opensource.org/licenses/Apache-2.0',
+      'GNU General Public': 'https://www.gnu.org/licenses/gpl-3.0',
+      'MIT': 'https://opensource.org/licenses/MIT',
+      'BSD 2-Clause': 'https://opensource.org/licenses/BSD-2-Clause',
+      'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
+      'Boost Software': 'https://opensource.org/licenses/BSL-1.0',
+      'Eclipse Public': 'https://opensource.org/licenses/EPL-1.0',
+      'Mozilla Public': 'https://opensource.org/licenses/MPL-2.0',
+      'The Unilicense': 'https://unlicense.org/',
     };
+  
     return licenseLinks[license] || '';
   }
+  
   function getLicenseSection(license) {
     const licenseSections = {
-        'Apache': 'Licensed under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0).',
-        'GNU General Public': 'Licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0).',
-        'MIT': 'Licensed under the [MIT License](https://opensource.org/licenses/MIT).',
-        // Add section content for other licenses
+      'Apache': 'Licensed under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0).',
+      'GNU General Public': 'Licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0).',
+      'MIT': 'Licensed under the [MIT License](https://opensource.org/licenses/MIT).',
+      'BSD 2-Clause': 'Licensed under the [BSD 2-Clause License](https://opensource.org/licenses/BSD-2-Clause).',
+      'BSD 3-Clause': 'Licensed under the [BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause).',
+      'Boost Software': 'Licensed under the [Boost Software License 1.0](https://opensource.org/licenses/BSL-1.0).',
+      'Eclipse Public': 'Licensed under the [Eclipse Public License 1.0](https://opensource.org/licenses/EPL-1.0).',
+      'Mozilla Public': 'Licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).',
+      'The Unilicense': 'Licensed under the [Unlicense](https://unlicense.org/).',
     };
+  
     return licenseSections[license] || '';
   }
+  
   function generateBadgeURLs(choices) {
     const badgeURLs = {
         'HTML': 'https://img.shields.io/badge/HTML-5-green',
@@ -36,33 +57,39 @@ function getBadgeURL(license) {
         'NoSQL': 'https://img.shields.io/badge/NoSQL-MongoDB-green',
         'React': 'https://img.shields.io/badge/React-17.x-blue',
     };
+  
     return choices.filter(choice => choice && badgeURLs[choice]).map(choice => {
       return `![${choice}](https://img.shields.io/badge/${encodeURIComponent(choice)}-blue)`;
     });
   }
+  
   // TODO: Create a function that returns a license badge based on which license is passed in
   // If there is no license, return an empty string
   function renderLicenseBadge(license) {
     const badgeURL = getBadgeURL(license);
     return badgeURL ? `![License](${badgeURL})` : '';
   }
+  
   // TODO: Create a function that returns the license link
   // If there is no license, return an empty string
   function renderLicenseLink(license) {
     const linkURL = getLicenseLinkURL(license);
     return linkURL ? `[${license} License](${linkURL})` : '';
     }
+  
   // TODO: Create a function that returns the license section of README
   // If there is no license, return an empty string
   function renderLicenseSection(license) {
     const sectionContent = getLicenseSection(license);
     return sectionContent ? `## License\n\n${sectionContent}` : '';
   }
+  
   // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
     // Initialize the Table of Contents string
     const badgeURLs = generateBadgeURLs(data.badges);
     let tableOfContents = '## Table of Contents\n\n';
+  
     // Check if each section has data and add it to the Table of Contents
     if (data.description) {
         tableOfContents += '- [Description](#description)\n';
@@ -88,8 +115,10 @@ function getBadgeURL(license) {
     if (data.github) {
         tableOfContents += '- [Questions](#questions)\n\n';
     }
+  
     // Generate the markdown content with the dynamic Table of Contents
     let markdownContent = `# ${data.title}\n\n${tableOfContents}`;
+  
     // Add other sections based on user input
     
     if (badgeURLs.length > 0) {
@@ -106,8 +135,8 @@ function getBadgeURL(license) {
         markdownContent += `## Credits\n\n${data.credits}\n\n`;
     }
     if (data.license) {
-        const licenseBadge = renderLicenseBadge(data.license);
-        const licenseLink = renderLicenseLink(data.license);
+      //   const licenseBadge = renderLicenseBadge(data.license);
+      //   const licenseLink = renderLicenseLink(data.license);
         const licenseSection = renderLicenseSection(data.license);
         markdownContent += `${licenseSection}\n\n`;
     }
@@ -126,4 +155,5 @@ function getBadgeURL(license) {
   
     return markdownContent;
   }
-  module.exports = generateMarkdown;
+  
+  module.exports = generateMarkdown;  
