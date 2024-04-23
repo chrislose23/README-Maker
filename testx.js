75dd34d132dddb9c1911417c11c18f440e0a1b41
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-04-23 00:15:42 -0400

    another test

--- testx.js
+++ testx.js
@@ -1,159 +1,157 @@
-function getBadgeURL(license) {
-    const licenseBadges = {
-      'Apache': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
-      'GNU General Public': 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
-      'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
-      'BSD 2-Clause': 'https://img.shields.io/badge/License-BSD%202--Clause-orange.svg',
-      'BSD 3-Clause': 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg',
-      'Boost Software': 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg',
-      'Eclipse Public': 'https://img.shields.io/badge/License-EPL%201.0-red.svg',
-      'Mozilla Public': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
-      'The Unilicense': 'https://img.shields.io/badge/license-Unlicense-blue.svg',
-    };
-  
-    return licenseBadges[license] || '';
-  }
-  
-  function getLicenseLinkURL(license) {
-    const licenseLinks = {
-      'Apache': 'https://opensource.org/licenses/Apache-2.0',
-      'GNU General Public': 'https://www.gnu.org/licenses/gpl-3.0',
-      'MIT': 'https://opensource.org/licenses/MIT',
-      'BSD 2-Clause': 'https://opensource.org/licenses/BSD-2-Clause',
-      'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
-      'Boost Software': 'https://opensource.org/licenses/BSL-1.0',
-      'Eclipse Public': 'https://opensource.org/licenses/EPL-1.0',
-      'Mozilla Public': 'https://opensource.org/licenses/MPL-2.0',
-      'The Unilicense': 'https://unlicense.org/',
-    };
-  
-    return licenseLinks[license] || '';
-  }
-  
-  function getLicenseSection(license) {
-    const licenseSections = {
-      'Apache': 'Licensed under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0).',
-      'GNU General Public': 'Licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0).',
-      'MIT': 'Licensed under the [MIT License](https://opensource.org/licenses/MIT).',
-      'BSD 2-Clause': 'Licensed under the [BSD 2-Clause License](https://opensource.org/licenses/BSD-2-Clause).',
-      'BSD 3-Clause': 'Licensed under the [BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause).',
-      'Boost Software': 'Licensed under the [Boost Software License 1.0](https://opensource.org/licenses/BSL-1.0).',
-      'Eclipse Public': 'Licensed under the [Eclipse Public License 1.0](https://opensource.org/licenses/EPL-1.0).',
-      'Mozilla Public': 'Licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).',
-      'The Unilicense': 'Licensed under the [Unlicense](https://unlicense.org/).',
-    };
-  
-    return licenseSections[license] || '';
-  }
-  
-  function generateBadgeURLs(choices) {
-    const badgeURLs = {
-        'HTML': 'https://img.shields.io/badge/HTML-5-green',
-        'CSS': 'https://img.shields.io/badge/CSS-3-blue',
-        'JavaScript': 'https://img.shields.io/badge/JavaScript-ES6-yellow',
-        'Node.js': 'https://img.shields.io/badge/Node.js-14.x-green',
-        'Express.js': 'https://img.shields.io/badge/Express.js-4.x-lightgrey',
-        'MySQL': 'https://img.shields.io/badge/MySQL-8.x-blue',
-        'NoSQL': 'https://img.shields.io/badge/NoSQL-MongoDB-green',
-        'React': 'https://img.shields.io/badge/React-17.x-blue',
-    };
-  
-    return choices.filter(choice => choice && badgeURLs[choice]).map(choice => {
-      return `![${choice}](https://img.shields.io/badge/${encodeURIComponent(choice)}-blue)`;
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const colors = require('colors');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?'
+    },
+    {
+        type: 'checkbox',
+        name: 'badges',
+        message: 'What code does your project utilize?',
+        choices: [
+            "HTML", 
+            "CSS", 
+            "JavaScript", 
+            "Node.js", 
+            "Express.js", 
+            "MySQL", 
+            "NoSQL", 
+            "React", 
+        ],
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project. (What, Why, and How)'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators and their GitHub profiles.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using for your project?',
+        choices: [
+            "Apache", 
+            "GNU General Public", 
+            "MIT", 
+            "BSD 2-Clause", 
+            "BSD 3-Clause", 
+            "Boost Software", 
+            "Eclipse Public", 
+            "Mozilla Public", 
+            "The Unilicense",
+            ""
+        ],
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can other developers contribute?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples of how to run any tests you created.'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the link to your GitHub Repo?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'usageInputs',
+        message: 'Provide instructions for usage:',
+    },
+    {
+        type: 'input',
+        name: 'usageScreenshot',
+        message: 'Provide a screenshot link:',
+        when: (answers) => answers.usageInputs !== undefined,
+    },
+    {
+        type: 'confirm',
+        name: 'addMoreUsage',
+        message: 'Do you want to add another usage input and screenshot?',
+        default: false,
+        when: (answers) => answers.usageInputs !== undefined, // Only ask if usage input is provided
+    },
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    console.log(colors.green('README.md has been generated sucessfully!'));
     });
-  }
-  
-  // TODO: Create a function that returns a license badge based on which license is passed in
-  // If there is no license, return an empty string
-  function renderLicenseBadge(license) {
-    const badgeURL = getBadgeURL(license);
-    return badgeURL ? `![License](${badgeURL})` : '';
-  }
-  
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
-  function renderLicenseLink(license) {
-    const linkURL = getLicenseLinkURL(license);
-    return linkURL ? `[${license} License](${linkURL})` : '';
-    }
-  
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
-  function renderLicenseSection(license) {
-    const sectionContent = getLicenseSection(license);
-    return sectionContent ? `## License\n\n${sectionContent}` : '';
-  }
-  
-  // TODO: Create a function to generate markdown for README
-  function generateMarkdown(data) {
-    // Initialize the Table of Contents string
-    const badgeURLs = generateBadgeURLs(data.badges);
-    let tableOfContents = '## Table of Contents\n\n';
-  
-    // Check if each section has data and add it to the Table of Contents
-    if (data.description) {
-        tableOfContents += '- [Description](#description)\n';
-    }
-    if (data.install) {
-        tableOfContents += '- [Installation](#installation)\n';
-    }
-    if (data.usageInputs) {
-        tableOfContents += '- [Usage](#usage)\n';
-    }
-    if (data.credits) {
-        tableOfContents += '- [Credits](#credits)\n';
-    }
-    if (data.license) {
-        tableOfContents += '- [License](#license)\n';
-    }
-    if (data.contribute) {
-        tableOfContents += '- [Contributing](#contributing)\n';
-    }
-    if (data.tests) {
-        tableOfContents += '- [Tests](#tests)\n';
-    }
-    if (data.github) {
-        tableOfContents += '- [Questions](#questions)\n\n';
-    }
-  
-    // Generate the markdown content with the dynamic Table of Contents
-    let markdownContent = `# ${data.title}\n\n${tableOfContents}`;
-  
-    // Add other sections based on user input
-    
-    if (badgeURLs.length > 0) {
-        const licenseBadge = renderLicenseBadge(data.license);
-        markdownContent += `## Description\n\n${badgeURLs.join(' ')} ${licenseBadge}\n\n${data.description}\n\n`;
-    }
-    if (data.install) {
-        markdownContent += `## Installation\n\n${data.install}\n\n`;
-    }
-    if (data.usageInputs) {
-        markdownContent += `## Usage\n\n${data.usageInputs}\n\n`;
-    }
-    if (data.credits) {
-        markdownContent += `## Credits\n\n${data.credits}\n\n`;
-    }
-    if (data.license) {
-      //   const licenseBadge = renderLicenseBadge(data.license);
-      //   const licenseLink = renderLicenseLink(data.license);
-        const licenseSection = renderLicenseSection(data.license);
-        markdownContent += `${licenseSection}\n\n`;
-    }
-    if (data.contribute) {
-        markdownContent += `## Contributing\n\n${data.contribute}\n\n`;
-    }
-    if (data.tests) {
-        markdownContent += `## Tests\n\n${data.tests}\n\n`;
-    }
-    if (data.github) {
-        markdownContent += `## Questions\n\n[My GitHub Repo](${data.github})\n`;
-    }
-    if (data.email) {
-      markdownContent += `\n[My Email Address](${data.email})\n\n`;
-  }
-  
-    return markdownContent;
-  }
-  
-  module.exports = generateMarkdown;  
+}
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        if (answers.usageInputs && answers.usageScreenshot) {
+            answers.usageInputs = `${answers.usageInputs}\n\n![Screenshot](${answers.usageScreenshot})`;
+        }
+        function askAdditionalUsage() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'usageInputs',
+                    message: 'Provide instructions for usage:',
+                },
+                {
+                    type: 'input',
+                    name: 'usageScreenshot',
+                    message: 'Provide a screenshot link:',
+                },
+                {
+                    type: 'confirm',
+                    name: 'addMoreUsage',
+                    message: 'Do you want to add another usage input and screenshot?',
+                    default: false,
+                },
+            ]).then((additionalAnswers) => {
+                // If usage inputs and screenshot are provided, combine them into a single string
+                if (additionalAnswers.usageInputs && additionalAnswers.usageScreenshot) {
+                    answers.usageInputs += `\n\n${additionalAnswers.usageInputs}\n\n![Screenshot](${additionalAnswers.usageScreenshot})`;
+                }
+                if (additionalAnswers.addMoreUsage) {
+                    askAdditionalUsage(); // Recursive call to continue asking for more
+                } else {
+                    // After all additional usage inputs and screenshots are collected, proceed with the remaining questions
+                    const markdown = generateMarkdown(answers);
+                    writeToFile('README.md', markdown);
+                }
+            });
+        }
+        // Start asking for additional usage inputs and screenshots
+        if (answers.addMoreUsage) {
+            askAdditionalUsage();
+        } else {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+        }
+    });
+}
+// Function call to initialize app
+init();
